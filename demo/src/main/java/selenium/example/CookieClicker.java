f0d2d0aa2ad6eaa2e8bc2c14c3c72064ee6b0a48
jay-brdy <jaysirit@gmail.com>
2024-08-11 22:55:02 -0500

    created infinite loop to click cookies and auto purchase upgrades

--- demo/src/main/java/selenium/example/CookieClicker.java
+++ demo/src/main/java/selenium/example/CookieClicker.java
@@ -13,10 +13,47 @@ public class CookieClicker {
         //navigate to webpage
         driver.get("https://cookieclicker.ee/");
 
+        //element IDs
+        String cookiesCountId = "cookies";
+        String productPricePrefix = "productPrice";
+        String productPrefix = "product";
+
         //find big cookie
         WebElement cookie = driver.findElement(By.id("bigCookie"));
 
         //click the element once
-        cookie.click();
+        // cookie.click();
+
+        //Infinite loop of clicking and upgrading
+        while (true) { 
+            //click cookie
+            cookie.click();
+
+            //get current cookie count
+            // splits by spaces, take first part which is the numerical value
+            String cookiesCountText = driver.findElement(By.id(cookiesCountId)).getText().split(" ")[0];
+            // removes any commas and converts the string into an integer
+            int cookiesCount = Integer.parseInt(cookiesCountText.replace(",", ""));
+            
+            //check products prices
+            for (int i = 0; i < 19; i++) {
+                //get product price
+                String productPriceText = driver.findElement(By.id(productPricePrefix + i)).getText().replace(",","");
+               
+                //if the product price is not a digit, continue to  next iteration
+                if (!productPriceText.matches("\\d+")) {
+                    continue;
+                }
+
+                int productPrice = Integer.parseInt(productPriceText);
+
+                //if we have enough cookies, buy the product
+                if (cookiesCount >= productPrice) {
+                    WebElement product = driver.findElement(By.id(productPrefix + i));
+                    product.click();
+                    break;
+                }
+            }
+        }
     }
 }
